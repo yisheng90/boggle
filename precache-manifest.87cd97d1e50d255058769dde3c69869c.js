2b4c63e793088785390bf7c7cba043d7790f1305
yisheng-homage <yisheng@homage.co>
2019-01-28 22:27:26 +0800

    Updates

---
+++ precache-manifest.87cd97d1e50d255058769dde3c69869c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e83ef22663b2dc7cbe02",
+    "url": "/boggle/static/css/main.0ed3c920.chunk.css"
+  },
+  {
+    "revision": "e83ef22663b2dc7cbe02",
+    "url": "/boggle/static/js/main.e83ef226.chunk.js"
+  },
+  {
+    "revision": "7c3aec79f3ae62290828",
+    "url": "/boggle/static/js/1.7c3aec79.chunk.js"
+  },
+  {
+    "revision": "cdc67b206a7a8d4e3594",
+    "url": "/boggle/static/js/runtime~main.cdc67b20.js"
+  },
+  {
+    "revision": "822d96fedc8213345626bae08ac5f5b7",
+    "url": "/boggle/index.html"
+  }
+];
