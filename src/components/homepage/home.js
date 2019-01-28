8b4815660f28bae70d75981b78ccf764fc097460
yisheng-homage <yisheng@homage.co>
2019-01-28 20:47:33 +0800

    add home component

--- src/components/homepage/home.js
+++ src/components/homepage/home.js
@@ -1,26 +1,18 @@
 import React, {Component} from 'react';
+import Logo from '../../elements/logo/logo';
+import Button from '../../elements/button/button'
 import './home.css';
 
 
 class HomePage extends Component {
-    render() {
-        return (
-            <div className="homePage">
-                <div className="logo">
-                    <span className="text-blue">B</span>
-                    <span className="text-yellow">o</span>
-                    <span className="text-blue">g</span>
-                    <span className="text-yellow">g</span>
-                    <span className="text-blue">l</span>
-                    <span className="text-yellow">e</span>
-                </div>
-
-                <div className="buttonWrapper">
-                   <button className="button">PLAY!</button>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="homePage">
+        <Logo/>
+        <Button title="PLAY!" onClickHandler={this.props.onClickHandler}/>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
