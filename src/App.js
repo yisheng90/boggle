c2b8cdf9bcf78c3adc6e2fc15942549ec9a0abfd
yisheng-homage <yisheng@homage.co>
2019-01-28 20:47:50 +0800

    update app component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, {Component} from 'react';
 import './App.css';
 import Game from './components/game/game';
-import HomePage from './components/homepage/home'
+import HomePage from './components/homepage/home';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.pages = [<HomePage />, <Game/>];
-        this.state = {
-            currentIdx: 0,
-        }
+  constructor(props) {
+    super(props);
+    this.pages = [, <Game/>];
+    this.state = {
+      currentIdx: 0,
+    };
+    this.nextPage = this._nextPage.bind(this);
+  }
+  
+  _nextPage() {
+    this.setState({currentIdx: 1});
+  }
+  
+  _renderPage() {
+    if (this.state.currentIdx === 0) {
+      return (<HomePage onClickHandler={this.nextPage}/>);
     }
-
-    _renderPage() {
-        return this.pages[this.state.currentIdx];
-    }
-
-    render() {
-        return (
-            <div className="App">
-                {this._renderPage()}
-            </div>
-        );
-    }
+    return (<Game/>);
+  }
+  
+  render() {
+    return (
+      <div className="app">
+        {this._renderPage()}
+      </div>
+    );
+  }
 }
 
 export default App;
